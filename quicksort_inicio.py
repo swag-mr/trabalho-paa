1fcee06a4f34a983ad5d1744672f0709fc95f16b
jhiy2004 <jhiy2004@gmail.com>
2024-10-27 16:58:56 -0300

    Implementando pilha no quicksort_inicio

--- quicksort_inicio.py
+++ quicksort_inicio.py
@@ -1,13 +1,16 @@
+from timeit import default_timer
+import random
+
 def particao(vetor, p, r):
     pivo = vetor[p]
     up = r
     down = p
-    while down < up :
-        while vetor[down] <= pivo:
+    while down < up:
+        while down < r and vetor[down] <= pivo:
             down = down+1
-        while vetor[up] > pivo:
+        while up > p and vetor[up] > pivo:
             up = up-1
-        if(down < up):
+        if (down < up):
             aux = vetor[down]
             vetor[down] = vetor[up]
             vetor[up] = aux
@@ -16,12 +19,31 @@ def particao(vetor, p, r):
     return up
 
 def quickSortInicio(vetor, p, r):
-    if(p < r):
-        q = particao(vetor, p, r)
-        quickSortInicio(vetor, p, q-1)
-        quickSortInicio(vetor, q+1, r)
+    stack = []
 
-x = [5,1,3,5,9,10,-1,110]
-quickSortInicio(x,0,len(x)-1)
+    stack.append(p)
+    stack.append(r)
 
-print(x)
+    while (len(stack) != 0):
+        temp_end = stack.pop()
+        temp_ini = stack.pop()
+
+        if (temp_ini < temp_end):
+            q = particao(vetor, temp_ini, temp_end)
+
+            stack.append(temp_ini)
+            stack.append(q-1)
+
+            stack.append(q+1)
+            stack.append(temp_end)
+
+x = random.sample(range(5500000),5500000)
+
+y = [i for i in range(100000)]
+
+inicio = default_timer()
+quickSortInicio(x, 0, len(x)-1)
+#quickSortInicio(y, 0, len(y)-1)
+fim = default_timer()
+
+print(f"Tempo: {fim - inicio}")
