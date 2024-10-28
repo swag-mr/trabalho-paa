4e9c9c3c6888e1050e5d9e8c122aa288466157f5
jhiy2004 <jhiy2004@gmail.com>
2024-10-28 13:19:30 -0300

    montando vetor de incrementos do shell_sort dentro da própria função de ordenar

--- shell_sort.py
+++ shell_sort.py
@@ -1,12 +1,23 @@
-def shell_sort(vetor, n, incrementos, numIncrementos):
+def shell_sort(vetor):
+    n = len(vetor)
+    incrementos = []
+    numIncrementos = 1
+
+    while (pow(2, numIncrementos) - 1 < n):
+        incrementos.append(pow(2, numIncrementos) - 1)
+        numIncrementos += 1
+
+    numIncrementos -= 1
+    print(incrementos)
+
     for i in range(0, numIncrementos):
         span = incrementos[i]
         for j in range(span, n):
             y = vetor[j]
 
-            k=j-span
+            k = j-span
 
-            while(k >= 0 and vetor[k] > y):
+            while (k >= 0 and vetor[k] > y):
                 vetor[k+span] = vetor[k]
                 k -= span
 
