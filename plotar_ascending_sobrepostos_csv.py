d96c1df1c49f3cd42d974c327d0ac885f5299f64
Vítor Moreira Rodrigues <vitorrdrgs04@gmail.com>
2024-10-31 09:23:16 -0300

    adicionado scripts para plotar os gráficos dos algoritmos que ficaram sobrepostos no gráfico com todos

---
+++ plotar_ascending_sobrepostos_csv.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter1d
+
+# rodar todos os arquivos csv e plotar em uma imagem só
+algorithms = [
+    # 'Bubble Sort',
+    'Bubble Sort Melhorado',
+    # 'Quick Sort (Início)',
+    'Quick Sort (Meio)',
+    'Insertion Sort',
+    'Shell Sort',
+    # 'Selection Sort',
+    'Heap Sort',
+    'Merge Sort'
+]
+
+for i in range(len(algorithms)):
+    df_plot = pd.read_csv(f'{algorithms[i]}.csv')
+    order_data = df_plot[df_plot["Ordem"] == 'ascending']
+    smooth_time = gaussian_filter1d(order_data["Tempo"], sigma=1)
+    plt.plot(order_data["Tamanho"], smooth_time, label=f'{algorithms[i]}')
+
+plt.title('Comparação de Desempenho - Dados Ascending (nlogn e n)')
+plt.xlabel('Tamanho do vetor')
+plt.ylabel('Tempo (segundos)')
+plt.legend()
+plt.grid(True)
+plt.show()
